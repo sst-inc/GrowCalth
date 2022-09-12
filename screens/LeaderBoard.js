70e9a4402190d5b0c1bf48c9da04c521939bf0e9
kidscoots101 <102847271+kidscoots101@users.noreply.github.com>
2022-09-12 19:40:37 +0800

    Update LeaderBoard.js

--- screens/LeaderBoard.js
+++ screens/LeaderBoard.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import { StyleSheet, Text, View, Dimensions, ImageBackground, SafeAreaView } from 'react-native'
+import { ScrollView } from 'react-native-gesture-handler';
 import { Circle } from 'react-native-progress';
 
 const LeaderBoard = () => {
@@ -14,6 +15,7 @@ const [leader, setLeader] = useState([])
 // On a side note, the leaderboards are not updated. This code is just an example of the UI for this page.
   const image = {uri : 'https://cdn-icons-png.flaticon.com/512/344/344682.png?w=360'}
     return (
+        <ScrollView>
         <View style={{backgroundColor: '#FFFF', flex: 1}}>
             {/* <Text style={{padding: 10, fontSize: 40, fontWeight: '900'}}>H</Text>
             <Text style={{padding: 10, fontSize: 40, fontWeight: '900'}}>O</Text>
@@ -24,28 +26,29 @@ const [leader, setLeader] = useState([])
             <View style={{backgroundColor: '#C1DBE3', width: windowWidth-35, height: 650, borderRadius: 20, alignSelf: 'center'}}>
                 <ImageBackground source={image} resizeMode="cover" style={styles.image}> 
             <View style={{flexDirection: 'row', alignSelf: 'center'}}>
-            <View style={{backgroundColor: 'white', height: 115, width: 115, borderRadius:(windowWidth-330/2), backgroundColor: yellow, right:18, justifyContent: 'center', alignItems: 'center', marginTop: -45}}>
+            <View style={{backgroundColor: 'white', height: 115, width: '35%', borderRadius:(windowWidth-330/2), backgroundColor: yellow, right:10, justifyContent: 'center', alignItems: 'center', marginTop: -45}}>
             <Text style={{color: 'black', fontWeight: '900', fontSize:45}}>127</Text>
             </View>
-            <View style={{backgroundColor: 'white', height: 115, width: 115, borderRadius:(windowWidth-300/2), backgroundColor: black, justifyContent: 'center' ,alignItems: 'center', marginTop: -110}}>
+            <View style={{backgroundColor: 'white', height: 115, width: '35%', borderRadius:(windowWidth-300/2), backgroundColor: black, justifyContent: 'center' ,alignItems: 'center', marginTop: -110}}>
                 <Text style={{color: 'white', fontWeight: '900', fontSize:45}}>192</Text>
             </View>
-                <View style={{backgroundColor: 'white', height: 115, width: 115, borderRadius:(windowWidth-300/2), backgroundColor: blue, right: -20, justifyContent: 'center', alignItems: 'center', marginTop: windowHeight-windowHeight-30, alignContent: 'space-around'}}>
+                <View style={{backgroundColor: 'white', height: 115, width: '35%', borderRadius:(windowWidth-300/2), backgroundColor: blue, right: -10, justifyContent: 'center', alignItems: 'center', marginTop: windowHeight-windowHeight-30, alignContent: 'space-around'}}>
                 <Text style={{color: 'white', fontWeight: '900', fontSize:45}}>111</Text>
                 </View>
             </View>
             </ImageBackground>
             
     <View style={{flexDirection: 'row', margin: 30,  alignItems: 'center'}}>     
-      <View style={{backgroundColor: 'white', height: 150, width: 150, borderRadius:(windowWidth-280/2), backgroundColor: green, marginTop: -20, left: 0 , justifyContent: 'center', alignItems: 'center', marginRight: 20}}>
+      <View style={{backgroundColor: 'white', height: 150, width: '50%', borderRadius:(windowWidth-280/2), backgroundColor: green, marginTop: -20, left: 0 , justifyContent: 'center', alignItems: 'center', marginRight: 20}}>
                 <Text style={{color: 'white', fontWeight: '900', fontSize:45}}>103</Text>
                 </View>
-                <View style={{backgroundColor: 'white', height: 150, width: 150, borderRadius:(windowWidth-280/2), backgroundColor: red, marginTop: -20, left: 0, justifyContent: 'center', alignItems: 'center'}}>
+                <View style={{backgroundColor: 'white', height: 150, width: '50%', borderRadius:(windowWidth-280/2), backgroundColor: red, marginTop: -20, left: 0, justifyContent: 'center', alignItems: 'center'}}>
                 <Text style={{color: 'white', fontWeight: '900', fontSize:45}}>94</Text>
                 </View>
             </View>
             </View> 
         </View>
+        </ScrollView>
     )
 }
 
