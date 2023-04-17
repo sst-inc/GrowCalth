84fbfc03c331a61b6383954ee42800ff0a05ad47
kidscoots101 <102847271+kidscoots101@users.noreply.github.com>
2023-04-17 19:52:44 +0800

    Update Admin.js

--- screens/AuthScreens/Admin.js
+++ screens/AuthScreens/Admin.js
@@ -1,24 +1,42 @@
 import React, {useState} from "react";
-import { StyleSheet, Text, View, Button, Touchable} from "react-native";
+import { StyleSheet, Text, View, Button, SafeAreaView} from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { auth } from "../../firebase";
 import { Input } from 'react-native-elements';
 import { db } from '../../firebase';
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { useNavigation } from "@react-navigation/native";
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+
+const Tabs = createMaterialTopTabNavigator();
+
+function MyTabs() {
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
+      <View style={{left: 10, top: 20}}>
+      <Text style={styles.title}>Admin Console</Text>
+      <Text style={styles.subtitle}>Welcome, Admin!</Text>
+      </View>
+    <Tabs.Navigator style={{top: 20}}> 
+      <Tabs.Screen name="Add Announcement" component={AddAnnouncement} />
+      <Tabs.Screen name="Add Event" component={AddEvent} />
+    </Tabs.Navigator>
+    </SafeAreaView>
+  );
+}
 
 const Tab = createBottomTabNavigator();
 
 const Admin = () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Dashboard" component={DashboardScreen} options={{headerShown: false}} />
+      <Tab.Screen name="Dashboard" component={MyTabs} options={{headerShown: false}} />
       <Tab.Screen name="Points" component={PointsScreen} options={{headerShown: false}}/>
     </Tab.Navigator>
   );
 };
 
-const DashboardScreen = () => {
+const AddAnnouncement = () => {
     const [header, setHeader] = useState('');
     const [text, setText] = useState('');
   
@@ -38,8 +56,6 @@ const DashboardScreen = () => {
     };
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Admin Console</Text>
-      <Text style={styles.subtitle}>Welcome, Admin!</Text>
       <Input
         label="Header"
         value={header}
@@ -62,6 +78,49 @@ const DashboardScreen = () => {
       </View>
     </View>
   );
+};
+const AddEvent = () => {
+  const [header, setHeader] = useState('');
+  const [text, setText] = useState('');
+
+  const handleAddItem = () => {
+    db.collection('Announcements').add({
+      header,
+      text,
+    })
+      .then(() => {
+        console.log('Item added successfully!');
+        setHeader('');
+        setText('');
+      })
+      .catch((error) => {
+        console.error('Error adding item: ', error);
+      });
+  };
+return (
+  <View style={styles.container}>
+    <Input
+      label="Header"
+      value={header}
+      onChangeText={setHeader}
+      placeholder="Enter header..."
+    />
+    <Input
+      label="Text"
+      value={text}
+      onChangeText={setText}
+      placeholder="Enter text..."
+    />
+    <View style={{alignSelf: 'center'}}>
+    <TouchableOpacity
+  onPress={handleAddItem}
+  style={{ backgroundColor: "#DB5461", borderRadius: 10, alignSelf: 'center' }}
+  >
+      <Text style={{color: 'white', fontWeight: 'bold', padding: 10, fontSize: 17}}>Add Event</Text>
+  </TouchableOpacity>
+    </View>
+  </View>
+);
 };
 
 const PointsScreen = () => {
@@ -86,7 +145,6 @@ const styles = StyleSheet.create({
         justifyContent: "flex-start",
         backgroundColor: "#fff",
         padding: 20,
-        paddingTop: 100,
       },
       title: {
         fontSize: 35,
